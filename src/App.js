b709977472e464c5f35baa909217b93a77603e3f
Damitht <tenna_koon@yahoo.com>
2024-07-18 19:33:00 +1000

    Finishing styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
 import Button from './Button';
 
 function App(){
+    const handleClick = () => {
+        console.log('handleClick !!!');
+    };
+
+    const handleMounseEnter = () => {
+        console.log('handleMounseEnter !!!');
+    };
+
+    const handleMouseLeave = () => {
+        console.log('handleMouseLeave !!!');
+    };
+
     return (<div>
         <div>
-            <Button primary rounded outline>Click Me !!!</Button>
+            <Button 
+            primary 
+            rounded 
+            outline 
+            className="mb-5"
+            onClick={handleClick}>
+                <GoBell />
+                Click Me !!!
+            </Button>
         </div>
         <div>
-            <Button secondary rounded outline>Buy now !</Button>
+            <Button 
+            secondary 
+            rounded 
+            outline 
+            onMouseEnter={handleMounseEnter}>
+                <GoCloudDownload />
+                Buy now !
+            </Button>
         </div>
         <div>
-            <Button success rounded outline>See Deal</Button>
+            <Button success rounded outline onMouseLeave={handleMouseLeave}>
+                <GoDatabase />
+                See Deal
+            </Button>
         </div>
         <div>
             <Button warning rounded outline>Hide Ads</Button>
--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { twMerge } from 'tailwind-merge';
 
 /*
 const finalClassName = classNames({
@@ -16,18 +17,28 @@ function Button({
     warning,
     danger,
     outline,
-    rounded
+    rounded,
+    ...rest
 }){
-    const classes = classNames('px-3 py-1.5 border',{
+    const classes = twMerge(
+        classNames(rest.className, 'flex items-center px-3 py-1.5 border',{
         'border-blue-500 bg-blue-500 text-white': primary === true,
         'border-gray-900 bg-gray-500 text-white': secondary,
         'border-green-500 bg-green-500 text-white': success,
         'border-yellow-400 bg-yellow-400 text-white': warning,
         'border-red-500 bg-red-500 text-white': danger,
-    });
+        'rounded-full': rounded,
+        'bg-white': outline,
+        'text-blue-500': outline && primary,
+        'text-gray-900': outline && secondary,
+        'text-green-500': outline && success,
+        'text-yellow-400': outline && warning,
+        'text-red-500': outline && danger,
+        })
+    );
 
     return (
-        <button className={classes}>
+        <button {...rest} className={classes}>
             { children}
         </button>
     );
